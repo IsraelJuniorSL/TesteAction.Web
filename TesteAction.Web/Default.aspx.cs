a6b359185864e8c73855bbd2a86db5997baf43a3
Israel Júnior <israeljunior@servicelogic.com.br>
2023-08-09 10:18:42 -0300

    First

---
+++ TesteAction.Web/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TesteAction.Application;
+
+namespace TesteAction.Web
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var classe = new Class1();
+            classe.Teste();
+        }
+    }
+}
---
+++ TesteAction.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TesteAction.Web.Global" Language="C#" %>
